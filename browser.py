71e9324f139642826e150b124f3c879c842d190b
Sopan Roy <sopan619@gmail.com>
2023-06-27 14:54:06 +0530

    Complete!!! Working, tested, will clear the code a bit in next commit

--- browser.py
+++ browser.py
@@ -1,23 +1,67 @@
 # Here we will try to launch a browser with a specified URL 
 import webbrowser, time, pyautogui
+import random
+import requests
 
-# Here we pass the link to open in a variable
-query = "python"
+# # Here we pass the link to open in a variable
+# query = "python"
 
-# Here is the URL after adding the query 
-finalUrl = ("https://www.bing.com/search?q=" + query)
+# # Here is the URL after adding the query 
+# finalUrl = ("https://www.bing.com/search?q=" + query)
 
 
 
-# Since, Edge is my default browser, it is opening EDGE
-webbrowser.open(finalUrl)
-time.sleep(4)
-pyautogui.hotkey("ctrl","w")
+# # Since, Edge is my default browser, it is opening EDGE
+# webbrowser.open(finalUrl)
+# time.sleep(4)
+# pyautogui.hotkey("ctrl","w")
 
-print("Job done!")
+# print("Job done!")
+
+# A function to get random words, got from internet
+
+
+def get_list_of_words():
+    response = requests.get(
+        'https://www.mit.edu/~ecprice/wordlist.10000',
+        timeout=10
+    )
+
+    string_of_words = response.content.decode('utf-8')
+
+    words = string_of_words.splitlines()
+
+    # words = get_list_of_words()
+    # print(words)
+
+    random_word = random.choice(words)
+    print(random_word)  # 👉️ zoo
+
+    url = ("https://www.bing.com/search?q=" + random_word)
+
+    webbrowser.open(url)
+    time.sleep(3)
+    pyautogui.hotkey("ctrl","w")
 
 
 # Now we have to make the searching part in a Function 
+
+# def search():
+#     # query = "random things here"
+#     url = ("https://www.bing.com/search?q=" + random_word)
+
+#     webbrowser.open(url)
+#     time.sleep(3)
+#     pyautogui.hotkey("ctrl","w")
+
+#     print(url)
+
+for _ in range(3):
+    get_list_of_words()
+    # search()
+    
+
+
 
 # Next we need to make a List of search terms to Loop through 
 
